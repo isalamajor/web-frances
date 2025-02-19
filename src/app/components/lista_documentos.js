70c5325961f357f4ee5ac48cfbd697ae051533c7
Sergi Vila <svila@pulsartec.com>
2025-02-19 14:23:41 +0100

    A ver si china gana a USA

--- src/app/components/lista_documentos.js
+++ src/app/components/lista_documentos.js
@@ -1,43 +1,27 @@
-import path from 'path';
+// src/app/components/lista_documentos.js
 import fs from 'fs';
+import path from 'path';
 
-export function generarListaDocumentos() {
-  const folderPath = path.join(process.cwd(), 'public', 'documentos');
-  const lista_documentos = [];
+const getDocumentos = () => {
+    const documentosPath = path.join(process.cwd(), 'public', 'documentos');
+    const archivos = fs.readdirSync(documentosPath);
 
-  // Función para generar el nombre, categoría y descripción
-  const generarDatosArchivo = (archivo) => {
-    const [nombre] = archivo.split('.');
-    const partes = nombre.split('_');
-    const categoria = partes[0];
-    const nombreCapitalizado = nombre.replace(/_/g, ' ').replace(/\b\w/g, (c) => c.toUpperCase());
-
-    return {
-      archivo,
-      nombre: nombreCapitalizado,
-      descripcion: `Documento de ${categoria}`,
-      imagen: 'archivo.png',
-      categoria,
-    };
-  };
-
-  // Verificar si la carpeta existe
-  if (fs.existsSync(folderPath)) {
-    const files = fs.readdirSync(folderPath).filter((file) => file.endsWith('.pdf'));
-    files.forEach((file, index) => {
-      const datosArchivo = generarDatosArchivo(file);
-      lista_documentos.push({
-        id: (index + 1).toString(),
-        ...datosArchivo,
-      });
+    const lista_documentos = archivos.map((archivo, index) => {
+        const nombreArchivo = archivo.replace(/_/g, ' ').replace('.pdf', '');
+        const categoria = archivo.split('_')[0];
+        return {
+            id: (index + 1).toString(),
+            archivo: archivo,
+            nombre: nombreArchivo,
+            descripcion: `Descripción de ${nombreArchivo}`,
+            imagen: "archivo.png",
+            categoria: categoria
+        };
     });
-  } else {
-    console.error('La carpeta "documentos" no existe.');
-  }
 
-  return lista_documentos;
-}
+    return lista_documentos;
+};
 
-// Exporta la lista generada para ser usada en otras partes de tu aplicación
-const lista_documentos = generarListaDocumentos();
+const lista_documentos = getDocumentos();
+
 export default lista_documentos;
